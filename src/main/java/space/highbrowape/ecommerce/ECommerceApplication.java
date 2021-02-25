58e9ae62f09a7be3d62379df25a0aa9bd0083626
sagarmachine <sagarmonkey800@gmail.com>
2021-02-25 14:44:32 +0530

    initial commit

---
+++ src/main/java/space/highbrowape/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package space.highbrowape.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+}
